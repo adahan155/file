5460e84301cea9d9aab02e5722b67b662848a65e
adahan155 <aidarbekovadahan8@gmail.com>
2023-02-25 17:56:02 +0300

    gght

---
+++ ClientServerSocked/SimplServer/src/Server.java
@@ -0,0 +1,30 @@
+import Aidarbekov.Phone;
+
+import java.io.*;
+import java.net.ServerSocket;
+
+
+public class Server {
+    public static <reader> void main(String[] args) {
+
+        try (ServerSocket server = new ServerSocket(8000))
+        {
+            System.out.println("Server started!");
+
+            while (true)
+                try ( Phone phone = new Phone(server)){
+
+                    String request = phone.readLine();
+                    System.out.println("Request:  " + request) ;
+                    String response =(int) (Math.random() * 30 - 10) + "";
+                    phone.writeLine(response);
+                    System.out.println("Response: "+ response);
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
